46f52cdd5123e2b9a2eb3fee6f7b8f9320030393
JoGCarC <jonatancarbajal19@gmail.com>
2026-04-27 22:51:43 -0500

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+
+st.title("Clase 4 Numpy")
+st.image("logo.png",width=100)
+st.sidebar.image("DMC.png")
+
+valor=st.slider("Seleccione un valor",1,20)
+
+arreglo=np.arange(valor)
+
+st.write(arreglo)
